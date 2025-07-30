e2b0eb4e21347cd2e3c97af3f2205b89856fe26f
Mathew Manoj (root) <64970593+mastermach50@users.noreply.github.com>
2025-07-31 00:31:15 +0530

    Merge remote-tracking branch 'origin/HEAD'

--- server.py
+++ server.py
@@ -7,9 +7,9 @@ ADDRESS = "0.0.0.0"
 
 class QwicClick(BaseHTTPRequestHandler):
 
-    def redirect_to_app(self):
+    def redirect_to(self, location):
         self.send_response(301)
-        self.send_header("Location", "https://app.qwic.click")
+        self.send_header("Location", location)
         self.send_header("EndServer", "QwicClick Redirection Server/0.0.1")
         self.end_headers()
         self.wfile.write(bytes("Redirecting", "utf-8"))
@@ -18,7 +18,10 @@ class QwicClick(BaseHTTPRequestHandler):
         return "QwicClick Redirection Server/0.0.1"
 
     def do_GET(self):
-        self.redirect_to_app()
+        if (self.path == "/design"):
+            self.redirect_to("https://www.figma.com/design/cP4r2oxky5hL0G0ThpUuGB/QwicClick?node-id=0-1&t=sYT4aplf9E7S5wTc-1")
+        else:
+            self.redirect_to("https://app.qwic.click")
 
 with HTTPServer((ADDRESS, PORT), QwicClick) as httpd:
     print(f"Serving on {ADDRESS}:{PORT}")
